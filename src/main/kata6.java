aee2ea65908e172b5fc8899906f0b548229fa611
Fernando <Fernando@LAPTOP-A70CUK09>
2021-12-30 13:05:19 +0000

    Kata5P2 versión 2

---
+++ src/main/kata6.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import toys.*;
+import toys.SerialNumberGenerator;
+
+public class kata6 {
+
+    public static void main(String[] args) {
+        SerialNumberGenerator serial =  new SerialNumberGenerator();
+        ArrayList<Car> cars = new ArrayList<>();
+        
+        System.out.print("Inserte instrucción: ");
+        Scanner input = new Scanner(System.in);
+        String string = "";
+        
+        while(!string.equals("exit")){
+            string = input.nextLine();       
+            
+            if (!string.equals("exit")){
+                Car car = new Car(serial.next());
+                car.pack();
+                car.label();
+                cars.add(car);
+                System.out.println("Producto procesado");
+                System.out.println("Para salir escirba 'exit'");
+            }
+        }
+    }
+    
+}
---
+++ src/toys/SerialNumberGenerator.java
@@ -0,0 +1,10 @@
+package toys;
+
+public class SerialNumberGenerator {
+    
+    private Integer serialNumber = 0;
+    
+    public int next() {
+        return serialNumber++;
+    }
+}
